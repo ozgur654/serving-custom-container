86312a3678f9b77400ed775e36f83f2a82c0998b
ozgur654 <52736716+ozgur654@users.noreply.github.com>
2023-07-25 21:03:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -12,26 +12,14 @@ from fastapi.responses import JSONResponse
 from transformers import DistilBertTokenizerFast
 from transformers import TFDistilBertForSequenceClassification
 
-tokenizer = DistilBertTokenizerFast.from_pretrained("distilbert-base-uncased")
-model = TFDistilBertForSequenceClassification.from_pretrained("../sentiment")
 
-app = FastAPI(title="Sentiment Analysis")
+model = AutoModelForCausalLM.from_pretrained("Ozgur98/pushed_model_mosaic_small", trust_remote_code=True).to(device='cuda:0', dtype=torch.bfloat16)
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
+
+app = FastAPI(title="LLM Model")
 
 AIP_HEALTH_ROUTE = os.environ.get('AIP_HEALTH_ROUTE', '/health')
 AIP_PREDICT_ROUTE = os.environ.get('AIP_PREDICT_ROUTE', '/predict')
-
-class Prediction(BaseModel):
-  sentiment: str 
-  confidence: Optional[float]
-
-class Predictions(BaseModel):
-    predictions: List[Prediction]
-
-# instad of creating a class we could have also loaded this information
-# from the model configuration. Better if you introduce new labels over time
-class Sentiment(Enum):
-  NEGATIVE = 0
-  POSITIVE = 1
 
 
 @app.get(AIP_HEALTH_ROUTE, status_code=200)
@@ -39,7 +27,7 @@ async def health():
     return {'health': 'ok'}
 
 @app.post(AIP_PREDICT_ROUTE, 
-          response_model=Predictions,
+          response_model,
           response_model_exclude_unset=True)
 async def predict(request: Request):
     body = await request.json()
@@ -51,30 +39,16 @@ async def predict(request: Request):
     instances = [x['text'] for x in instances]
     print(instances)
 
-    tf_batch = tokenizer(instances, max_length=128, padding=True,
-                            truncation=True, return_tensors='tf')
+    tokenized_example = tokenizer(data, return_tensors='pt')
+    outputs = model.generate(tokenized_example['input_ids'].to('cuda:0'), max_new_tokens=100, do_sample=True, top_k=10, top_p = 0.95)
 
-    print(tf_batch)
-
-    tf_outputs = model(tf_batch)
-
-    print(tf_outputs)
-
-    tf_predictions = tf.nn.softmax(tf_outputs[0], axis=-1)
-    print(tf_predictions)
-
-    indices = np.argmax(tf_predictions, axis=-1)
-    confidences = np.max(tf_predictions, axis=-1)
-
-    outputs = []
-
-    for index, confidence in zip(indices, confidences):
-      sentiment = Sentiment(index).name
-      print(index)
-      print(confidence)
-      outputs.append(Prediction(sentiment=sentiment, confidence=confidence))
-
-    return Predictions(predictions=outputs)
+    # Postprocess
+    answer = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    prompt = answer[0].rstrip()
+    
+    return prompt
+  
+    
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0",port=8080)
